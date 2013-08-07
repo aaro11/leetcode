a15ac7e68d3713c2f6427f2334a27c925311b171
Yulong Liu <lyl890808@gmail.com>
2013-08-07 14:51:25 +0800

    11

--- Binary Tree Maximum Path Sum.cpp
+++ Binary Tree Maximum Path Sum.cpp
@@ -7,31 +7,27 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-int result = 0;
-int search(TreeNode *root){
-    int value = 0;
-    if (!root)
-        return 0;
-    
-    int left = search(root->left);
-    int right = search(root->right);
-    
-    if (left + right + root->val > result){
-        result = left + right + root->val;
-    }
-    
-    int val = (left > right ? left : right) + root->val;
-    return val > 0 ? val : 0;
-}
- 
 class Solution {
 public:
     int maxPathSum(TreeNode *root) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        result = -1000000;
-        search(root);
-        
+        result = INT_MIN;
+        pathsum(root);
         return result;
     }
+    
+    int pathsum(TreeNode *root){
+        if (!root)  return 0;
+        int left = pathsum(root->left);
+        int right = pathsum(root->right);
+        
+        if (left + root->val + right > result)
+            result = left + root->val + right;
+            
+        int val = (left > right ? left : right) + root->val;
+        return val > 0 ? val : 0;
+    }
+private:
+    int result;
 };
